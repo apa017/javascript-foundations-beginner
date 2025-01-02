185c4c21d7db690d748055889104f631698e570f
Andrea Paviglianiti <69317115+apa017@users.noreply.github.com>
2025-01-02 10:25:06 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+let firstName = 'Tobias'
+let yearsOld = 15
+
+
+console.log('Hello World')
+console.log("I'm a simple log message for the browser console.")
+console.log("My name is", firstName)
+console.log("I am " + yearsOld + " years old")
