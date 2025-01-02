f1eb2648873a6a708d8f4cd0c4448c9ceb5b6884
Andrea Paviglianiti <69317115+apa017@users.noreply.github.com>
2025-01-02 11:03:53 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// Input Variable
+let firstName = prompt('Please enter your first name')
+
+// Output variable to html
+document.getElementById('greetings').innerHTML = 'My name is ' + firstName
+
+// Testing variables in console
+console.log(firstName)
