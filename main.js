878a20c05168e42ce7a23c4ccaf5b90d48718cf8
Andrea Paviglianiti <69317115+apa017@users.noreply.github.com>
2025-01-02 11:30:17 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-// Input Variable
-let firstName = prompt('Please enter your first name')
+// Create Array
+let friends = ['Beni', 'Scotty', 'Rocky', 'Lucky']
 
-// Output variable to html
-document.getElementById('greetings').innerHTML = 'My name is ' + firstName
 
-// Testing variables in console
-console.log(firstName)
+// test 1: Output in console
+console.log(friends)
+
+// test 2: Output in console: specific value by index
+console.log(friends[0])
+
+// test 3: Output in console: use loop to print every value in array (index + value)
+for (let item in friends) {console.log(item + ' : ' + friends[item])}
+
+
+
+// Modify array: add a new element
+friends[4] = 'Billo'
+
+
+// test 4: Output in console: print modified array
+console.log(friends)
+
+
+
+// Modify array: change value by its index
+friends[0] = 'Bee'
+
+// test 5: Output in console: print modified array
+console.log(friends)
